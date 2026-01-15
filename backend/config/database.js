9fd2c0e4cdcaf5f49d3d0a49c721eb56288fcc13
zulfikar <zulfikarmuhamad207@gmail.com>
2026-01-15 16:55:13 +0700

    Update version json backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,14 +2,13 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // Logic: Kalau ada DATABASE_URL (Online), pakai itu. Kalau gak ada, error.
-const db = new Sequelize(process.env.DATABASE_URL, {
+const db = new Sequelize('postgresql://neondb_owner:.....@ep-bold-....aws.neon.tech/neondb?sslmode=require', {
     dialect: 'postgres',
     protocol: 'postgres',
-    logging: false, // Biar terminal gak berisik
     dialectOptions: {
         ssl: {
             require: true,
-            rejectUnauthorized: false // Wajib buat Neon/Render/Vercel
+            rejectUnauthorized: false
         }
     }
 });
