3bb9b73f452aeafa241edf90169daeabecbdad76
zulfikar <zulfikarmuhamad207@gmail.com>
2026-01-15 17:35:47 +0700

    h

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-// LINK BERSIH (Tanpa channel_binding) -> INI PASTI JALAN DI VERCEL
+// LINK DB BERSIH (Tanpa channel_binding)
+// Ini sudah gue sesuaikan sama punya lu. Langsung copas aja.
 const db = new Sequelize('postgresql://neondb_owner:npg_4LyVuErGcqS1@ep-bold-haze-a1vqhfhi-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require', {
     dialect: 'postgres',
     protocol: 'postgres',
