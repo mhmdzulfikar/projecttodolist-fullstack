594b7874f91fd6ff1bdf8191ff5c0aca27868fb5
zulfikar <zulfikarmuhamad207@gmail.com>
2026-01-15 17:16:15 +0700

    Rename database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 
 // KONEKSI DATABASE (Versi Hardcode untuk Debugging)
 // Ini link asli lu yang udah dibersihin. Jangan diubah-ubah dulu.
-const db = new Sequelize('postgresql://neondb_owner:npg_4LyVuErGcqS1@ep-bold-haze-a1vqhfhi-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require', {
+const db = new Sequelize('postgresql://neondb_owner:npg_4LyVuErGcqS1@ep-bold-haze-a1vqhfhi-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', {
     dialect: 'postgres',
     protocol: 'postgres',
     logging: false,
